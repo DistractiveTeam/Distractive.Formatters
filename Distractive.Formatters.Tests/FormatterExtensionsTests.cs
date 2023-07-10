a25a979d01857a8b7965d5efc2a02f20893ae7c9
Tia <pakpoom@ookbee.com>
2023-07-10 21:29:09 +0700

    เพิ่ม extension method ToThaiWords() กับ ToBahtText()

---
+++ Distractive.Formatters.Tests/FormatterExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Distractive.Formatters.Tests
+{
+    public class FormatterExtensionsTests
+    {
+        [Fact]
+        public void ExtensionTests()
+        {
+            Assert.Equal("หนึ่งบาทถ้วน", 1M.ToBahtText());
+            Assert.Equal("หนึ่ง", 1L.ToThaiWords());
+            Assert.Equal("หนึ่ง", 1.ToThaiWords());
+        }
+    }
+}
---
+++ Distractive.Formatters/FormatterExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distractive.Formatters
+{
+    public static class FormatterExtensions
+    {
+        private static readonly ThaiNumberTextFormatter _formatter = new ThaiNumberTextFormatter();
+
+        public static string ToThaiWords(this long value) => _formatter.Format(value);
+        public static string ToThaiWords(this int value) => ToThaiWords((long)value);
+
+        public static string ToBahtText(this decimal value) => _formatter.GetBahtText(value);
+    }
+}
